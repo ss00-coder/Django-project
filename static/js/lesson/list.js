b2020ce2fca026813f703e8a3742abd540c21bae
heesu0_0 <ss00.coder@gmail.com>
2023-08-03 17:40:59 +0900

    lesson/list 작업완료

--- static/js/lesson/list.js
+++ static/js/lesson/list.js
@@ -1,83 +1,10 @@
-// $('.filter-button').on('click', () => {
-//   console.log("클릭");
-//   ;
-// });
+/* 카테고리 필터 */
+$(".filter-modal").hide();
 
-// $('.category-select').on('focusout', () => {
-//   $('.filter-modal').css('display', 'none');
-// });
+$('.filter-button').on('click', () => {
+  $(".filter-modal").show();
+});
 
-function showCatagoryFilter() {
-  console.log("함수");
-  if ($('#modal')) {
-    return;
-  }
-  const modal = `<div id="modal">
-    <div class="category-select">
-      <div class="category-wrapper">
-        <h4 class="category-title">언어 설정</h4>
-        <div style="height: 24px"></div>
-        <div class="category-tag-container">
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">한국어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">영어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">일본어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">중국어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">독일어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">스페인어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">프랑스어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">스웨덴어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">베트남어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">태국어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">그리스어</span>
-          </label>
-          <label class="category-tag">
-            <input type="checkbox" class="category-input" />
-            <span class="category-tag-name">러시아어</span>
-          </label>
-        </div>
-        <div style="height: 24px"></div>
-        <button type="button" class="category-apply">적용</button>
-      </div>
-    </div>
-    <div class="filter-blur"></div>
-  </div>`;
-  $('.filter-modal').append(modal);
-}
-
-// $('body').click(function (e) {
-//   if (!$('.filter-modal').find(e.target).length) {
-//     $('#modal').remove();
-//   }
-// });
+$('.filter-blur').on('click', () => {
+  $(".filter-modal").hide();
+});
