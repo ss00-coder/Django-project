ebbd6f4d84f5ccb1186c89fb2126bcbd563a6701
heesu0_0 <ss00.coder@gmail.com>
2023-08-06 22:16:14 +0900

    게시판 완료

---
+++ static/js/event/list.js
@@ -0,0 +1,12 @@
+/* lesson/list.html */
+
+/* 카테고리 필터 */
+$(".filter-modal").hide();
+
+$('.filter-button').on('click', () => {
+  $(".filter-modal").show();
+});
+
+$('.filter-blur').on('click', () => {
+  $(".filter-modal").hide();
+});
