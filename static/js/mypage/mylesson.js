6323d03a90e2d10a6d1a67856af1366151d82de2
dev-hyeon1022 <hited0123@gmail.com>
2023-08-12 18:18:58 +0900

    마이페이지 새로운  구조 설계완료

---
+++ static/js/mypage/mylesson.js
@@ -0,0 +1,35 @@
+
+// 페이지 로드완료
+$(function () {
+    // 객체들
+    const $tabBtn = $('.tab-item-btn');
+    const $totalNumber = $('.total-number');
+    const $deleteBtn = $('.delete-button');
+    const $modal = $('.modal');
+    const $buttonCancel = $('.button-cancel');
+    const $buttonAgree = $('.button-agree');
+
+    // 해당 작성글 전체 수 최초 값 설정
+    $totalNumber.text($tabBtn.eq(0).find('.tab-number').text())
+
+    // 탭 버튼 이벤트
+    $tabBtn.click((e) => {
+        if (!$(e.currentTarget).attr('class').includes('active-tab')) {
+            $tabBtn.toggleClass("active-tab");
+            $('.tab-line').remove();
+            $(e.currentTarget).append('<div class="tab-line"></div>');
+
+            // 해당 작성글 전체 수 변경하는 이벤트
+            $totalNumber.text($(e.currentTarget).find('.tab-number').text());
+        }
+    })
+
+    //모달창 이벤트
+    $deleteBtn.click(() => {$modal.show()});
+    $buttonCancel.click(() => {$modal.hide()})
+    $buttonAgree.click(() => {$modal.hide()})
+});
+
+
+
+
