1cb5a2201d099c1f9d2ec579edb2735d2c428e19
heesu0_0 <ss00.coder@gmail.com>
2023-08-16 00:56:46 +0900

    게시판 detail 완료

---
+++ Foreign_us/static/js/event/detail.js
@@ -0,0 +1,22 @@
+/* event/detail.html */
+$('.comment-textarea-wrapper').hide();
+$('.comment-write-with-login').on('click', () => {
+  $('.comment-write-with-login').hide();
+  $('.comment-textarea-wrapper').show();
+});
+$('button.cancel-button').on('click', () => {
+  $('.comment-write-with-login').show();
+  $('.comment-textarea-wrapper').hide();
+});
+
+$(".heart").each((i, heart) => {
+  $(heart).on('click', () => {
+    $(".heart").removeClass("active");
+    if(i==0){
+      $(".heart").eq(1).addClass("active");
+    } else {
+      $(".heart").eq(0).addClass("active");
+    }
+  })
+  
+})
