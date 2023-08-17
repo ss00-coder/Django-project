1fc448bfb4373d36cd733b3c5268771472716270
DKemma <test.eastshine@gmail.com>
2023-08-17 23:58:45 +0900

    마이페이지 백

--- Foreign_us/mypage/tests.py
+++ Foreign_us/mypage/tests.py
@@ -1,3 +1,45 @@
+import os
+import django
+from django.db.models import Q, Count
 from django.test import TestCase
 
+from lesson.models import Lesson
+from member.models import Member
+
 # Create your tests here.
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = 'true'
+django.setup()
+
+
+# class MemberTest(TestCase):
+#     # 클래스 내부에 코드를 작성하면, 커밋 된다.
+#     # 하지만 메소드 내에서 코드를 작성하면, 롤백 된다.
+#
+#     # ======================================
+#     # bulk_create
+#     # ======================================
+#
+#     Member.objects.bulk_create([
+#         Member(member_email='test55@gmail.com', member_nickname='석'),
+#         Member(member_email='test95@naver.com', member_nickname='길동'),
+#         Member(member_email='test3@naver.com', member_nickname='짱구'),
+#         Member(member_email='test4@google.com', member_nickname='철수'),
+#         Member(member_email='test5@nate.com', member_nickname='유리'),
+#     ])
+
+
+class Lesson(TestCase):
+
+    Lesson.objects.bulk_create([
+        Lesson(post_title='영어', post_content='영어과외', post_view_count=12, post_status='Y', member_id=1),
+        Lesson(post_title='일본어', post_content='일본어과외',post_view_count=142, post_status='Y', member_id=2),
+        Lesson(post_title='중국어', post_content='중국어과외', post_view_count=24, post_status='N', member_id=3),
+        Lesson(post_title='독일어', post_content='독일어과외', post_view_count=19, post_status='Y', member_id=4),
+        Lesson(post_title='영어', post_content='영어과외', post_view_count=124, post_status='N', member_id=5),
+        Lesson(post_title='일본어', post_content='일본어과외', post_view_count=122, post_status='Y', member_id=5),
+        Lesson(post_title='중국어', post_content='중국어과외', post_view_count=95, post_status='N', member_id=1),
+        Lesson(post_title='독일어', post_content='독일어과외', post_view_count=42, post_status='Y', member_id=1),
+    ])
+
--- Foreign_us/mypage/views.py
+++ Foreign_us/mypage/views.py
@@ -1,16 +1,45 @@
+from django.db.models import F
 from django.shortcuts import render
 from django.views import View
+
+from lesson.models import Lesson
+from member.models import Member
 
 
 # Create your views here.
 class MyProfileView(View):
+    # 프로필 사이드바 닉네임
     def get(self, request):
-        return render(request, 'mypage/myprofile.html')
+        # email = request.session['member_email'] #(멤버이메일값)
+        # print(Member.objects.annotate(email=F('member_email')).values('member_nickname'))
+        # member = Member.objects.get(member_email=email)
+        member = Member.objects.filter(member_nickname='짱구')
+        # print(member[0].member_nickname)  // 이건 필터
+        nicknames = {'nickname':member[0].member_nickname}
+        # print(nicknames)
+        # print(nicknames)
+        return render(request, 'mypage/myprofile.html', nicknames)
+
+
+    # def post(self, request, *args, **kwargs):
+    #     token, member_email, member_nickname, member_birth = request.POST.values()
+    #     Member.objects.create(member_email=member_email, member_password=member_password, member_name=member_name,
+    #                           member_age=member_age, member_birth=member_birth)
+    #     # 이동할 urls 경로를 작성한다.
+    #     return redirect('/member/login')
 
 
 class MyLessonView(View):
     def get(self, request):
-        return render(request, 'mypage/mylesson.html')
+        five_lessons =Lesson.objects.all()[0:5]
+        print(five_lessons)
+
+        context = {
+            "lessons" : five_lessons
+        }
+
+        return render(request, 'mypage/mylesson.html', context)
+
 
 
 class MyLessonReviewView(View):
