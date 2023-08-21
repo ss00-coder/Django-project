355f65178f0036c82e84ad7ff73e4c98972b28a8
heesu0_0 <ss00.coder@gmail.com>
2023-08-21 17:14:24 +0900

    notice/detail replies

--- Foreign_us/notice/urls.py
+++ Foreign_us/notice/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 
 from event.views import EventListView, EventDetailView, EventWriteView
 from helpers.views import HelpersListView, HelpersDetailView, HelpersWriteView
-from notice.views import NoticeListView, NoticeDetailView, NoticeListAPI
+from notice.views import NoticeListView, NoticeDetailView, NoticeListAPI, NoticeReplyListAPI, NoticeReplyWriteAPI, \
+    NoticeReplyModifyAPI, NoticeReplyDeleteAPI
 
 app_name = 'notice'
 
 urlpatterns = [
+    # 공지사항
     path('list/', NoticeListView.as_view(), name='list-init'),
     path('list/<int:page>', NoticeListAPI.as_view(), name='list'),
     path('detail/<int:post_id>', NoticeDetailView.as_view(), name='detail'),
+    # 공지사항 댓글
+    path('replies/list/<int:post_id>', NoticeReplyListAPI.as_view(), name='list-init'),
+    path('replies/list/<int:post_id>/<int:page>', NoticeReplyListAPI.as_view(), name='list'),
+    path('replies/write/', NoticeReplyWriteAPI.as_view(), name='write'),
+    path('replies/modify/', NoticeReplyModifyAPI.as_view(), name='modify'),
+    path('replies/delete/', NoticeReplyDeleteAPI.as_view(), name='delete_post'),
+    path('replies/delete/<int:id>/', NoticeReplyDeleteAPI.as_view(), name='delete_get'),
 ]
--- Foreign_us/notice/views.py
+++ Foreign_us/notice/views.py
@@ -8,8 +8,9 @@ from django.views import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from notice.models import Notice, NoticeFile
-from notice.serializers import NoticeSerializer
+from member.models import Member
+from notice.models import Notice, NoticeFile, NoticeReply
+from notice.serializers import NoticeSerializer, NoticeReplySerializer
 
 
 class NoticeListView(View):
@@ -25,7 +26,15 @@ class NoticeListAPI(APIView):
         # post = Notice.objects.filter(id=post_id).annotate(member_nickname=F('member__member_nickname'),
         #                                                   post_file=Subquery(post_file.values('image')[:1])).values(
         #     'id', 'post_title', 'post_content', 'post_view_count', 'post_status', 'member_nickname', 'post_file')
-        posts = list(Notice.objects.order_by('-id').all())[offset:limit + 1]
+        all_posts = list(Notice.objects.order_by('-id').all())
+        posts = []
+        for i in range(len(all_posts)):
+            id = all_posts[i].id
+            notice_file = NoticeFile.objects.filter(notice_id=id)
+            post = Notice.objects.filter(id=id).annotate(post_file=notice_file.values('image')[:1]).values('id', 'post_title', 'post_content', 'post_file')
+            posts.append(post)
+
+        posts = posts[offset:limit + 1]
         # posts = list(Notice.objects.order_by('-id').all())[offset:limit + 1]
         hasNext = False
 
@@ -35,7 +44,8 @@ class NoticeListAPI(APIView):
 
 
         context = {
-            'posts': NoticeSerializer(posts, many=True).data,
+            # 'posts': NoticeSerializer(posts, many=True).data,
+            'posts': posts,
             'hasNext': hasNext
         }
 
@@ -52,9 +62,68 @@ class NoticeDetailView(View):
         context = {
             'post': post,
             'post_files': list(post.noticefile_set.all()),
-            'member': post.member
+            'writer': post.member,
+            'member': Member.objects.get(member_email=request.session['member_email'])
         }
 
         return render(request, 'notice/detail.html', context)
 
 
+
+# 댓글
+class NoticeReplyListAPI(APIView):
+    def get(self, request, post_id, page=1):
+        size = 5
+        offset = (page - 1) * size
+        limit = page * size
+        replies = list(NoticeReply.objects.filter(notice_id=post_id).order_by("-id").annotate(member_nickname=F('member__member_nickname')).values('id', 'reply_content', 'member_nickname', 'created_date'))
+        total = len(replies);
+
+        replies = replies[offset:limit + 1]
+        # posts = list(Notice.objects.order_by('-id').all())[offset:limit + 1]
+        hasNext = False
+
+        if len(replies) > size:
+            hasNext = True
+            replies.pop(size)
+
+        context = {
+            # 'posts': NoticeSerializer(posts, many=True).data,
+            'replies': replies,
+            'hasNext': hasNext,
+            'total': total
+        }
+        return Response(context)
+        # return Response(NoticeReplySerializer(replies, many=True).data)
+
+
+class NoticeReplyWriteAPI(APIView):
+    def post(self, request):
+        datas = request.data
+        datas = {
+            'notice': Notice.objects.get(id=datas.get('post_id')),
+            'member': Member.objects.get(member_email=request.session['member_email']),
+            'reply_content': datas.get('reply_content')
+        }
+        NoticeReply.objects.create(**datas)
+        return Response('success')
+
+
+class NoticeReplyModifyAPI(APIView):
+    def post(self, request):
+        datas = request.data
+
+        NoticeReply.objects.filter(id=datas['id']).update(reply_content=datas['reply_content'])
+        return Response('success')
+
+
+class NoticeReplyDeleteAPI(APIView):
+    def get(self, request, id):
+        print(id)
+        NoticeReply.objects.filter(id=id).delete()
+        return Response('success')
+
+    def post(self, request):
+        datas = request.data
+        NoticeReply.objects.get(id=datas['id']).delete()
+        return Response('success')
